bc55ac895e353f562f151a0bf684cffe6af292a3
sayalibytes <sayalimehtayvr@gmail.com>
2024-06-06 00:43:36 +0530

    Configure environment variables for Heroku

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 require('dotenv').config();
-const { PORT, BASE_URL } = process.env;
+const PORT = process.env.PORT || 5050;
 
 app.use(express.json());
 app.use(cors());
@@ -12,5 +12,5 @@ app.get("/", (req, res) => {
 });
 
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${BASE_URL}${PORT}`);
+  console.log(`Server is running successfully`);
 });
