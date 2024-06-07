67da51cc79530e37ad23097bcc405c282c29a666
sayalibytes <sayalimehtayvr@gmail.com>
2024-06-08 01:39:08 +0530

    add console log to check get error

--- server/routes/questionnaire.js
+++ server/routes/questionnaire.js
@@ -5,14 +5,21 @@ const path = require('path');
 
 // Function to read a JSON file and parse its contents
 const readJsonFile = (filePath) => {
+    console.log(`Reading file from path: ${filePath}`);
   const data = fs.readFileSync(path.join(__dirname, '..', filePath), 'utf-8');
   return JSON.parse(data);
 };
 
 // Handle the API endpoint to get the questionnaire data
 router.get('/', (req, res) => {
-  const data = readJsonFile('data/questions.json');
-  res.json(data);
-});
-
+    console.log('Received GET request at /questionnaire');
+    try {
+        const data = readJsonFile('data/questions.json');
+        res.json(data);
+      } catch (error) {
+        console.error('Error reading questionnaire data:', error);
+        res.status(500).send('Internal Server Error');
+      }
+    });
+    
 module.exports = router;
