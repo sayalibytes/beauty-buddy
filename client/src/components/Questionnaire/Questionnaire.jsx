dc0eb4cc00533d465db2f7599bc0110cf85d2b45
sayalibytes <sayalimehtayvr@gmail.com>
2024-06-09 12:21:23 +0530

    debug the followup question error

--- client/src/components/Questionnaire/Questionnaire.jsx
+++ client/src/components/Questionnaire/Questionnaire.jsx
@@ -6,14 +6,16 @@ import "./Questionnaire.scss";
 function Questionnaire({ setSkinType }) {
   const [questions, setQuestions] = useState([]);
   const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
-  const [followUpIndex, setFollowUpIndex] = useState(-1);
-  const [answers, setAnswers] = useState([]);
+  const [followUpQuestions, setFollowUpQuestions] = useState([]);
+  const [currentFollowUpIndex, setCurrentFollowUpIndex] = useState(-1);
+  //   const [answers, setAnswers] = useState([]);
   const Navigate = useNavigate();
 
   useEffect(() => {
     const fetchQuestions = async () => {
       try {
         const response = await getQuestionnaire();
+        console.log("Questions fetched:", response.data.questions);
         setQuestions(response.data.questions);
       } catch (error) {
         console.error("Error fetching questions:", error);
@@ -25,31 +27,42 @@ function Questionnaire({ setSkinType }) {
 
   const handleOptionClick = (option) => {
     const currentQuestion = questions[currentQuestionIndex];
+    console.log("Current Question:", currentQuestion);
 
     if (currentQuestion.id === 3 && option === "I don't know") {
-      setFollowUpIndex(0);
-    } else {
-      if (followUpIndex >= 0 && currentQuestion.followup_questions) {
-        determineSkinType(option);
-        if (followUpIndex < currentQuestion.followup_questions.length - 1) {
-          setFollowUpIndex(followUpIndex + 1);
-        } else {
-          setFollowUpIndex(-1);
-          setCurrentQuestionIndex(currentQuestionIndex + 1);
-        }
+      if (
+        currentQuestion.followup_questions &&
+        currentQuestion.followup_questions.length > 0
+      ) {
+        setFollowUpQuestions(currentQuestion.followup_questions);
+        setCurrentFollowUpIndex(0);
       } else {
-        if (currentQuestion.id === 3) {
-          setSkinType(option);
-        }
-        setAnswers([
-          ...answers,
-          { questionId: currentQuestion.id, answer: option },
-        ]);
-        if (currentQuestionIndex < questions.length - 1) {
-          setCurrentQuestionIndex(currentQuestionIndex + 1);
-        } else {
-          Navigate("/get-started");
-        }
+        console.error("Follow-up questions are not defined for question id 3");
+      }
+    } else if (currentFollowUpIndex >= 0) {
+      determineSkinType(option);
+      if (currentFollowUpIndex < followUpQuestions.length - 1) {
+        setCurrentFollowUpIndex(currentFollowUpIndex + 1);
+      } else {
+        setFollowUpQuestions([]);
+        setCurrentFollowUpIndex(-1);
+        moveToNextQuestion();
+      }
+    } else {
+      if (currentQuestion.id === 3) {
+        setSkinType(option);
+      }
+      moveToNextQuestion();
+    }
+  };
+
+  const moveToNextQuestion = () => {
+    if (currentFollowUpIndex === -1) {
+      // setAnswers((prevAnswers) => [...prevAnswers, { questionId: currentQuestion.id, answer: option }]);
+      if (currentQuestionIndex < questions.length - 1) {
+        setCurrentQuestionIndex(currentQuestionIndex + 1);
+      } else {
+        Navigate("/get-started");
       }
     }
   };
@@ -75,11 +88,10 @@ function Questionnaire({ setSkinType }) {
     setSkinType(determinedSkinType);
   };
   const getCurrentQuestion = () => {
-    const currentQuestion = questions[currentQuestionIndex];
-    if (followUpIndex >= 0 && currentQuestion.followup_questions) {
-      return currentQuestion.followup_questions[followUpIndex];
+    if (currentFollowUpIndex >= 0 && followUpQuestions.length > 0) {
+      return followUpQuestions[currentFollowUpIndex];
     }
-    return currentQuestion;
+    return questions[currentQuestionIndex];
   };
 
   const currentQuestion = getCurrentQuestion();
@@ -88,7 +100,9 @@ function Questionnaire({ setSkinType }) {
     <div className="questionnaire">
       {questions.length > 0 && currentQuestion && (
         <>
-          <h2 className="questionnaire__title">{currentQuestion.question}</h2>
+          <h2 className="questionnaire__title">
+            {currentQuestion.question || currentQuestion.followup_question}
+          </h2>
           <div className="questionnaire__options">
             {currentQuestion.options.map((option, index) => (
               <button
